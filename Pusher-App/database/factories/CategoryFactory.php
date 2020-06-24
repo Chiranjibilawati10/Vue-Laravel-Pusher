2711723a1066384fa143d59ed9e9aa333a0893e9
chiranjibi.lawati@infodeveloper.com.np <chiranjibilawati08@gmail.com>
2020-06-24 09:51:45 +0545

    CRUD for questions

--- Pusher-App/database/factories/CategoryFactory.php
+++ Pusher-App/database/factories/CategoryFactory.php
@@ -2,11 +2,12 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model\Category;
 use Faker\Generator as Faker;
 
-$factory->define(Category::class, function (Faker $faker) {
+$factory->define(App\Model\Category::class, function (Faker $faker) {
+    $word = $faker->word;
     return [
-        //
+        'name' => $word,
+        'slug' => Str::slug($word)
     ];
 });
